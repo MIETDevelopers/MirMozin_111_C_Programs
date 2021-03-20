d1a0eb27fec63113f9eaf1f47345fddb91ef3065
Mir <75358919+justmirr@users.noreply.github.com>
2021-03-20 17:40:24 +0530

    Sum of Two Numbers

--- 2_Sum.c
+++ 2_Sum.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
-int main(){
-	int number1, number2, sum;
-	scanf("%d %d", &number1, &number2);
+/*
+Author: Mir Saeed Mozin
+Creation Date; 15th March 2021
+Program purpose: To calculate the sum of two entered numbers
+*/
+#include<stdio.h> //Pre-processor directive to include standard input and output functions header file
+int main(){ //Main function
+	int number1, number2, sum; //Integer variable declaration
+	scanf("%d %d", &number1, &number2); //Scanf function calling for taking 2 inputs
 	sum=number1+number2;
-	printf("The Sum of the two numbers = %d", sum);
-	return 0;
+	printf("The Sum of the two numbers = %d", sum); //For printing the calculated sum
+	return 0; //Return function
 }
