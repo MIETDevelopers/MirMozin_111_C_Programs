59d2dfca91bf06f3726b44c040414fc5f507434d
Mir <75358919+justmirr@users.noreply.github.com>
2021-03-25 02:19:03 +0530

    Calculating Factorial of an Entered Number

---
+++ 09_Factorial.c
@@ -0,0 +1,19 @@
+/*
+Author: Mir Saeed Mozin
+Creation Date; 16th March 2021
+Program purpose: To generate factorial of an entered number
+*/
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+int main(){ //Main function
+ 	int n1=1, number; //Integer variable declaration
+ 	printf("Enter an integer: "); //Printf and Scanf function calling function calling
+  	scanf("%d", &number);
+ 	printf("The factorial of %d is: \n", number);
+ 	//For loop starting from 1 upto the entered number
+  	for(int i=1; i<=number; i++)
+  	{
+      	n1 = n1*i;
+ 		printf("%d\t", n1);
+ 	}
+ 	return 0; //Return function
+}
