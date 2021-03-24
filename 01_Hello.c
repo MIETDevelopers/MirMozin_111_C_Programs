a8485f67f3abe95d6a6b4cf5eea1bf70efc2d2ee
Mir <75358919+justmirr@users.noreply.github.com>
2021-03-25 02:09:23 +0530

    Hello World Program

---
+++ 01_Hello.c
@@ -0,0 +1,10 @@
+/*
+Author: Mir Saeed Mozin
+Creation Date; 15th March 2021
+Program purpose: To print 'Hello World'
+*/
+#include <stdio.h> //Pre-processor directive to include standard input and output functions header file
+int main(){ //Main Function
+	printf("Hello World\n"); //Printf function calling 
+	return 0; //Return function
+}
