149aabd396d7789e2bcb7fee3d6862b5ec3139e1
Mir <75358919+justmirr@users.noreply.github.com>
2021-03-29 12:20:33 +0530

    Use of String Operations

--- 20_String_Operations.c
+++ 20_String_Operations.c
@@ -1,13 +1,14 @@
 /*
 Author: Mir Saeed Mozin
 Creation Date: 21st March 2021
-Program purpose: To demonstrate the use of various string operations
+Program purpose: To demonstrate the use of string operations
 */
 #include<stdio.h> //Pre-processor directive to include standard input and output functions header file
 int main(void){
-	char name[] = "hippopotamus";
-	printf("%c\t%c\t%c\t", *name, *(name+2), *(name+4)); //Output will be: h    p    o
-	char *namePtr;
-	namePtr = name;
-	printf("\n%c\t%c\t%c\t", *namePtr, *(namePtr+3), *(namePtr+5)); //Output will be: h    p    p
+	char name[30];
+    	printf("Enter name: ");
+	gets(name);     //Function to read string from user
+	printf("Name: ");
+	puts(name);    //Function to display string
+	return 0;    //Return function
 }
