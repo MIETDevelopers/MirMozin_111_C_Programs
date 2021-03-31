69bb9ebc9c5f0cddb5d9cdfbe068f34911bbba06
Mir <75358919+justmirr@users.noreply.github.com>
2021-03-31 11:36:46 +0530

    Switch Case Calculator

--- 21_Switch_Case.c
+++ 21_Switch_Case.c
@@ -17,19 +17,19 @@ int main(){ //Main function
     //Switch case for performing calculation and printing the result based on the entered operator and numbers
     switch (operator) {
         case '+': //Addition case
-	    printf("%.1lf + %.1lf = %.1lf", number1, number2, number1+number2);
-	    break;
-	case '-': //Subtraction case
-	    printf("%.1lf - %.1lf = %.1lf", number1, number2, number1-number2);
-	    break;
-    	case '*': //Multiplication case
-	    printf("%.1lf * %.1lf = %.1lf", number1, number2, number1*number2);
-	    break;
-    	case '/': //Division case
-	    printf("%.1lf / %.1lf = %.1lf", number1, number2, number1/number2);
-	    break;
-    	default: //Default case if an incorrect operator is entered
-	    printf("Error! Wrong operator");
+            printf("%.1lf + %.1lf = %.1lf", number1, number2, number1+number2);
+            break;
+        case '-': //Subtraction case
+            printf("%.1lf - %.1lf = %.1lf", number1, number2, number1-number2);
+            break;
+        case '*': //Multiplication case
+            printf("%.1lf * %.1lf = %.1lf", number1, number2, number1*number2);
+            break;
+        case '/': //Division case
+            printf("%.1lf / %.1lf = %.1lf", number1, number2, number1/number2);
+            break;
+        default: //Default case if an incorrect operator is entered
+            printf("Error! Wrong operator");
     }
     return 0; //Return function
 }
